8ed1c862962670abc2d388749b9d960dc5b9a2d0
freedomkhanyile <freedom.khanyile1@gmail.com>
2019-01-16 15:10:22 +0200

    routing works fine

--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -2,13 +2,25 @@ import { LoginComponent } from "./login/login.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home.component";
+import { IndexComponent } from "./index";
+import { OurServicesComponent } from "./our-services";
+import { AboutUsComponent } from "./about-us";
+import { ContactUsComponent } from "./contact-us";
 
-export const declarations = [HomeComponent, LoginComponent];
+export const declarations = [HomeComponent, LoginComponent, IndexComponent, OurServicesComponent, AboutUsComponent, ContactUsComponent];
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
-    children: [{ path: "login", component: LoginComponent }]
+    children: 
+    [ 
+      { path: "", component: IndexComponent },      
+      { path: "login", component: LoginComponent},
+      { path: "our-services", component: OurServicesComponent},
+      { path: "about-us", component: AboutUsComponent}, 
+      { path: "contact-us", component: ContactUsComponent},     
+    
+    ]
   }
 ];
 
