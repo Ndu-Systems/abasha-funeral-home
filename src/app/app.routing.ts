0c69d4b139681df26c3fd6e9b49c7e5a243e3370
freedomkhanyile <freedom.khanyile1@gmail.com>
2019-01-25 13:54:54 +0200

    How can we help

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NavBarComponent } from './nav-bar';
 import { HomePageComponent } from './home-page';
+import { HelpYouComponent } from './home-page/help-you';
 
 const routes: Routes = [];
 
-export const declarations = [NavBarComponent, HomePageComponent];
+export const declarations = [
+  NavBarComponent, 
+  HomePageComponent,
+  HelpYouComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule] 
+  exports: [RouterModule]
 })
 export class AppRouting { }
