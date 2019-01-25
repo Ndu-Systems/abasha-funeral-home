1ed5a0b77b99d34d7cd12ae7dccc348f1bfffb8e
freedomkhanyile <freedom.khanyile1@gmail.com>
2019-01-25 14:43:34 +0200

    our benefits done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { NavBarComponent } from './nav-bar';
 import { HomePageComponent } from './home-page';
 import { HelpYouComponent } from './home-page/help-you';
+import { BenefitsComponent } from './home-page/benefits';
 
 const routes: Routes = [];
 
 export const declarations = [
   NavBarComponent, 
   HomePageComponent,
-  HelpYouComponent
+  HelpYouComponent,
+  BenefitsComponent
 ];
 
 @NgModule({
